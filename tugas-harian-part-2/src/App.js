eaf49a86ee01ca8c93c4f389e9644236701c1034
iim mafahir <7200022-iim-mafahir@users.noreply.gitlab.com>
2021-05-17 20:37:24 +0700

    hasil panggilan tugas 11

--- tugas-harian-part-2/src/App.js
+++ tugas-harian-part-2/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Tugas9 from "./Tugas-9/tugas9"
 // soal 2
 import Tugas10 from "./Tugas-10/ListBuah"
+import Tugas11 from "./Tugas-11/tugas11"
 
 //import logo from './logo.svg';
 //import './App.css';
@@ -13,6 +14,7 @@ function App(){
     <>
       <Tugas9/> 
       <Tugas10/> 
+      <Tugas11/>
     </>
   )
 }
