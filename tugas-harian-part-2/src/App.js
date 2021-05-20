4df4b5fe81149a50961e909f76476ef3a34eb7c2
iim mafahir <7200022-iim-mafahir@users.noreply.gitlab.com>
2021-05-21 02:27:26 +0700

    memanggil tugas 14

--- tugas-harian-part-2/src/App.js
+++ tugas-harian-part-2/src/App.js
@@ -4,7 +4,9 @@ import React from 'react';
 //import Tugas10 from "./Tugas-10/ListBuah"
 //import Tugas11 from "./Tugas-11/tugas11"
 //import Tugas12 from "./Tugas-12/tugas12"
-import Tugas13 from "./Tugas-13/tugas13"
+//import Tugas13 from "./Tugas-13/tugas13"
+import Tugas14 from "./Tugas-14/DaftarMahasiswa"
+
 
 //import logo from './logo.svg';
 //import './App.css';
@@ -13,7 +15,7 @@ function App(){
 
   return (
     <>
-      <Tugas13/>
+      <Tugas14/>
     </>
   )
 }
